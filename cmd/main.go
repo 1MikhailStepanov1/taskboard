c149ef77a80358f47c7167640ef081611db0ee98
1MikhailStepanov1 <wakamail923@gmail.com>
2025-06-24 10:30:47 +0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	localLogsLevel      = "LOCAL"
+	productionLogsLevel = "PROD"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case localLogsLevel:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case productionLogsLevel:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func main() {
+	
+}
